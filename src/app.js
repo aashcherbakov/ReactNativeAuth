39b0ffc4a49436b85a5132dccb60dc70dd4b2a80
Alexander Shcherbakov <oo.shcherbakov@gmail.com>
2017-02-01 09:57:19 -0500

    Add login form

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './component/common';
+import LoginForm from './component/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCnQe3h64vxgj346gYoPdv5jMV80uL4TPU',
+      authDomain: 'auth-5c286.firebaseapp.com',
+      databaseURL: 'https://auth-5c286.firebaseio.com',
+      storageBucket: 'auth-5c286.appspot.com',
+      messagingSenderId: '552630458779'
+    });
+  }
+
   render() {
     return(
       <View>
-        <Text>An App!</Text>
+        <Header headerText='Authentication' />
+        <LoginForm />
       </View>
     );
   };
